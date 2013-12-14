4535fd6a594d73fde923da80022efc98b0533fc7
Adam Whittingham <adam.whittingham@gmail.com>
2013-12-14 22:19:13 +0000

    Wrap install in a function to avoid closed connection issues when piping curl into bash

--- install.sh
+++ install.sh
@@ -4,46 +4,50 @@ install_version=$1
 elixir_version=${install_version:-'0.11.2'}
 elixir_base=/opt/elixir
 
-echo "Installing Elixir ${elixir_version} to ${elixir_base}"
+function install_elixir {
+  echo "Installing Elixir ${elixir_version} to ${elixir_base}"
 
-# Lets make our new folder and move in
-sudo mkdir $elixir_base
-sudo chown $USER $elixir_base
-pushd $elixir_base >/dev/null
+  # Lets make our new folder and move in
+  sudo mkdir $elixir_base
+  sudo chown $USER $elixir_base
+  pushd $elixir_base >/dev/null
 
-# Right, lets start off with the bare essentials
-sudo apt-get install build-essential wget
+  # Right, lets start off with the bare essentials
+  sudo apt-get install build-essential wget
 
-# Elixir runs on the Erlang VM, so lets add the erlang repos...
-wget http://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
-sudo dpkg -i erlang-solutions_1.0_all.deb
-rm erlang-solutions_1.0_all.deb
+  # Elixir runs on the Erlang VM, so lets add the erlang repos...
+  wget http://packages.erlang-solutions.com/erlang-solutions_1.0_all.deb
+  sudo dpkg -i erlang-solutions_1.0_all.deb
+  rm erlang-solutions_1.0_all.deb
 
-#... and install Erlang
-sudo apt-get update
-sudo apt-get install esl-erlang
+  #... and install Erlang
+  sudo apt-get update
+  sudo apt-get install esl-erlang
 
-# Now for the fun stuff, lets get Elixir
-wget https://github.com/elixir-lang/elixir/archive/v${elixir_version}.tar.gz
-tar xf v${elixir_version}.tar.gz
-rm v${elixir_version}.tar.gz
-cd ${elixir_base}/elixir-${elixir_version}
-make clean test
+  # Now for the fun stuff, lets get Elixir
+  wget https://github.com/elixir-lang/elixir/archive/v${elixir_version}.tar.gz
+  tar xf v${elixir_version}.tar.gz
+  rm v${elixir_version}.tar.gz
+  cd ${elixir_base}/elixir-${elixir_version}
+  make clean test
 
-# We'll add a 'current' symlink to make it easier to rollback
-cd $elixir_base
-ln -sf elixir-${elixir_version} current
+  # We'll add a 'current' symlink to make it easier to rollback
+  cd $elixir_base
+  ln -sf elixir-${elixir_version} current
 
-# At last,  we can just add it to the path!
-source_script=/var/tmp/elixir
-cat >$source_script <<EOF
-export ELIXIR_HOME=${elixir_base}/current
-export PATH=\$ELIXIR_HOME/bin:\$PATH
+  # At last,  we can just add it to the path!
+  source_script=/var/tmp/elixir
+  cat >$source_script <<-EOF
+  export ELIXIR_HOME=${elixir_base}/current
+  export PATH=\$ELIXIR_HOME/bin:\$PATH
 EOF
-chmod a=rx $source_script
-sudo mv $source_script /etc/profile.d/elixir
+  chmod a=rx $source_script
+  sudo mv $source_script /etc/profile.d/elixir
+
+  popd >/dev/null
+}
+
+install_elixir
 
 # Source our new file in this shell so we're ready to rock!
 source /etc/profile.d/elixir
-
-popd >/dev/null
